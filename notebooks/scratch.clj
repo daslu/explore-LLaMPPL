ba3ab7a887cea64ebbbdbe9735dead4a85b7bfb7
daslu <daniel.slutsky@gmail.com>
2023-11-05 14:00:34 +0200

    added initial llama.clj usage

---
+++ notebooks/scratch.clj
@@ -0,0 +1,52 @@
+(ns scratch
+  (:require [instaparse.core :as insta]
+            [com.phronemophobic.llama :as llama]
+            [com.phronemophobic.llama.raw :as raw]
+            [com.phronemophobic.llama.util :as llutil]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [tech.v3.datatype.functional :as fun]
+            [tech.v3.datatype.argops :as argops]))
+
+;; copied from the original llamma.clj tutorials
+
+(defonce llama7b-path "/workspace/models/llama-2-7b-chat.ggmlv3.q4_0.bin")
+(defonce llama-context (llama/create-context llama7b-path {}))
+
+(def token->str
+  (into (sorted-map)
+        (comp (map
+               (fn [token]
+                 [token (raw/llama_token_to_str llama-context token)]))
+              (take-while (fn [[token untoken]]
+                            untoken)))
+        (range 0 Integer/MAX_VALUE)))
+
+(defonce previous* (atom nil))
+
+
+(defn get-logits [ctx s]
+  (raw/llama_set_rng_seed ctx 1234)
+  (cond
+
+    (string? s)
+    (llama/llama-update ctx s 0)
+
+    (vector? s)
+    (let [prev @previous*]
+      (if (and
+           (vector? prev)
+           (= prev (butlast s)))
+        (llama/llama-update ctx (last s))
+        (do
+          (llama/llama-update ctx (llama/bos) 0)
+          (run! #(llama/llama-update ctx %)
+                s)))))
+  (reset! previous* s)
+
+  (into [] (llama/get-logits ctx)))
+
+(->> "Clojure is a"
+     (get-logits llama-context)
+     argops/argmax
+     token->str)
