e7a2ec94788a4457cb8c9d2b0c32434f7be66704
daslu <daniel.slutsky@gmail.com>
2023-12-19 14:05:03 +0200

    experimenting with cache

---
+++ notebooks/cache.clj
@@ -0,0 +1,96 @@
+(ns cache
+  (:require [instaparse.core :as insta]
+            [com.phronemophobic.llama :as llama]
+            [com.phronemophobic.llama.raw :as raw]
+            [com.phronemophobic.llama.util :as llutil]
+            [com.phronemophobic.llama.impl.model :as model]
+            [clojure.java.io :as io]
+            [clojure.string :as string]
+            [tech.v3.datatype :as dtype]
+            [tech.v3.datatype.functional :as fun]
+            [tech.v3.datatype.argops :as argops]
+            [tech.v3.dataset.print]
+            [tablecloth.api :as tc]))
+
+
+(defn now []
+  (java.util.Date.))
+
+;; copied from the original llamma.clj tutorials
+
+(defonce llama7b-path "/workspace/models/llama-2-7b-chat.ggmlv3.q4_0.bin")
+(defonce llama-context (llama/create-context llama7b-path {}))
+(def llama-eos (llama/eos llama-context))
+
+(def token->str
+  (into (sorted-map)
+        (comp (map
+               (fn [token]
+                 [token (raw/llama_token_to_str llama-context token)]))
+              (take-while (fn [[token untoken]]
+                            untoken)))
+        (range 0 Integer/MAX_VALUE)))
+
+(defonce previous* (atom nil))
+(defn get-logits [ctx s]
+  #_(raw/llama_set_rng_seed ctx 1234)
+  (cond
+
+    (string? s)
+    (llama/llama-update ctx s 0)
+
+    (vector? s)
+    (let [prev @previous*]
+      (if (and
+           (vector? prev)
+           (= prev (butlast s)))
+        (llama/llama-update ctx (last s))
+        (do
+          (llama/llama-update ctx (llama/bos) 0)
+          (run! #(llama/llama-update ctx %)
+                s)))))
+  (reset! previous* s)
+
+  (into [] (llama/get-logits ctx)))
+
+(delay
+  (->> ["Good morning."]
+       (get-logits llama-context)
+       argops/argmax
+       token->str))
+
+(delay
+  (->> ["Good morning."
+        "What is good?"]
+       (get-logits llama-context)
+       argops/argmax
+       token->str))
+
+
+(defn test-texts [text1 text2]
+  (prn [:----------------])
+  (time
+   (let [ctx (time (llama/create-context llama7b-path {}))]
+     (time (raw/llama_set_rng_seed ctx 1234))
+     (prn :bos)
+     (time (llama/llama-update ctx (llama/bos) 0))
+     (let [_ (prn text1)
+           _ (time (llama/llama-update ctx text1))
+           x (-> ctx
+                 llama/get-logits
+                 argops/argmax
+                 token->str)
+           _ (prn text2)
+           _ (time (llama/llama-update ctx text2))
+           y(-> ctx
+                llama/get-logits
+                argops/argmax
+                token->str)]
+       (prn [:----------------])
+       [x y]))))
+
+(test-texts "Good Morning, my"
+            "Good Morning, my")
+
+(test-texts "Good Morning, my"
+            "Good Morning, my dear")
