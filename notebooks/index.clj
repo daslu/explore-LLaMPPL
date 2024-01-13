8af985675eac5e471a46e46764d16675a3ba639e
daslu <daniel.slutsky@gmail.com>
2024-01-13 23:02:50 +0200

    Clay setup

---
+++ notebooks/index.clj
@@ -0,0 +1,8 @@
+^{:clay {:quarto {:format {:html {:toc true
+                                  :toc-depth 4
+                                  :theme [:spacelab "notebooks/custom.scss"]}}
+                  :highlight-style :solarized
+                  :code-block-background true
+                  :include-in-header {:text "<link rel = \"icon\" href = \"data:,\" />"}
+                  :title "exploring LLaMPPL in Clojure"}}}
+(ns index)
